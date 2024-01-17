31036acff1813fbbcff1183f55c246159c557746
usmankadai <usmankadai@gmail.com>
2024-01-17 13:55:58 +0000

    remove old code

--- uopsb-app/app/page.tsx
+++ uopsb-app/app/page.tsx
@@ -1,10 +1,5 @@
 import Nav from '../components/Nav'
 
-const metadata = {
-    title: "UOP Study Buddy",
-    description: "Author: Taylor McFarlane",
-  };
-  
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
