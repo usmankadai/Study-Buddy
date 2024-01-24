08178770c7435aafe70efb73619d2c7431c52acc
usmankadai <usmankadai@gmail.com>
2024-01-24 23:44:40 +0000

    update db to handle initial sign up form #23

--- uopsb-app/server/migrations-sqlite/v1_user_login.sql
+++ uopsb-app/server/migrations-sqlite/v1_user_login.sql
@@ -1,9 +1,21 @@
 CREATE TABLE IF NOT EXISTS student (
-  id VARCHAR(36) NOT NULL PRIMARY KEY,
+  id VARCHAR(36) PRIMARY KEY,
   email VARCHAR(255) NOT NULL,
   fname VARCHAR(255) NOT NULL,
   lname VARCHAR(255) NOT NULL,
-  picture VARCHAR(255), 
+  picture VARCHAR(255),
+  course_code VARCHAR(12) NOT NULL,
+  gender TEXT CHECK( gender IN ('Male', 'Female', 'Other', 'Prefer not to say') ) NOT NULL, -- enum psql
+  year INTEGER NOT NULL,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  FOREIGN KEY (course_code) REFERENCES course(code)
 );
+
+CREATE TABLE IF NOT EXISTS course (
+    code VARCHAR(12) PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    department VARCHAR(50) NOT NULL
+);
+
+INSERT INTO course (code, name, department) VALUES ('U0968PYC', 'Software Engineering', 'School of Computing');
