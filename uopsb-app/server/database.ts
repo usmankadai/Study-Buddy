297ad10f2cd051724597d6cd649bf5da5699e2a7
usmankadai <usmankadai@gmail.com>
2024-01-24 23:42:01 +0000

    update db to handle initial sign up form #23

--- uopsb-app/server/database.ts
+++ uopsb-app/server/database.ts
@@ -1,6 +1,7 @@
 import BetterSqlite3 from "better-sqlite3";
 import fs from "fs";
 import path from "path";
+import { Course, FormPopulation, CreateUserType } from "@/app/types";
 
 // Connecting to or creating a new SQLite database file
 function init() {
@@ -9,7 +10,7 @@ function init() {
     process.cwd(),
     "server",
     "migrations-sqlite",
-    "v1_user_login.sql",
+    "v1_user_login.sql"
   );
   const migrationSql = fs.readFileSync(migrationFile, "utf8");
 
@@ -18,24 +19,22 @@ function init() {
 }
 const dbConn = init();
 
-export async function createUser(creds: {
-  email: string;
-  family_name: string;
-  given_name: string;
-  picture: string;
-}) {
+export async function createUser(creds: CreateUserType) {
   const db = await dbConn;
+  console.log(creds);
   const email = creds.email;
   const upNum = creds.email.match(/\d+/); // Extract the UP number email
   const fname = creds.given_name;
   const lname = creds.family_name;
   const picture = creds.picture;
-  const date = new Date().toISOString();
+  const year = creds.year;
+  const courseCode = creds.course;
+  const gender = creds.gender;
   const stmnt = await db
     .prepare(
-      `INSERT INTO student (id, email, fname, lname, picture) VALUES (?, ?, ?, ?, ?)`,
+      `INSERT INTO student (id, email, fname, lname, picture, year, course_code, gender) VALUES (?, ?, ?, ?, ?, ?, ?, ?)`
     )
-    .run(upNum, email, fname, lname, picture);
+    .run(upNum, email, fname, lname, picture, year, courseCode, gender);
   if (stmnt.changes !== 1) throw new Error("Could not create user");
   return true;
 }
@@ -46,4 +45,13 @@ export async function getUserByEmail(email: string) {
     .prepare("SELECT * FROM student WHERE email = ?")
     .get(email);
   return user;
+}
+
+export async function getFormPopulation() {
+  const db = await dbConn;
+  const courses = (await db.prepare("SELECT * FROM course").all()) as Course[];
+  const formPopulation: FormPopulation = {
+    courses,
+  };
+  return formPopulation;
 }
