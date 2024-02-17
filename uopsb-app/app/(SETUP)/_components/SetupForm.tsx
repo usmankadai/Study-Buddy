7d96b2cf91e90bcc9872658923ff1d1e82ddd261
usmankadai <usmankadai@gmail.com>
2024-02-17 14:43:31 +0000

    add next and back buttons to form

--- uopsb-app/app/(SETUP)/_components/SetupForm.tsx
+++ uopsb-app/app/(SETUP)/_components/SetupForm.tsx
@@ -2,7 +2,7 @@
 import { useAuth } from "@/app/AuthContext";
 import { jwtDecode } from "jwt-decode";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 import { useFormik } from "formik";
 import {
@@ -77,6 +77,7 @@ const handleSubmit = async (
 export function SetupForm(formPopulation: FormPopulation) {
   const { token, setIsLoggedIn } = useAuth();
   const router = useRouter();
+  const [step, setStep] = useState(0);
   const user = token ? jwtDecode(token as string) : null;
   const initialValues: SetupFormInitValues = {
     year: "",
@@ -102,84 +103,112 @@ export function SetupForm(formPopulation: FormPopulation) {
       onSubmit={formik.handleSubmit}
       className="bg-white p-6 rounded-md shadow-xl"
     >
-      <div className="mb-4">
-        <label htmlFor="year" className="block text-purple-700 font-bold mb-2">
-          University Year
-        </label>
-        <select
-          id="year"
-          name="year"
-          value={formik.values.year}
-          onChange={formik.handleChange}
-          className="w-full p-2 border border-purple-300 rounded-md focus:border-purple-500 focus:outline-none"
-        >
-          <option value="">Select a year</option>
-          <option value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-        </select>
-      </div>
+      {step === 0 && (
+        <>
+          <div className="mb-4">
+            <label
+              htmlFor="year"
+              className="block text-purple-700 font-bold mb-2"
+            >
+              University Year
+            </label>
+            <select
+              id="year"
+              name="year"
+              value={formik.values.year}
+              onChange={formik.handleChange}
+              className="w-full p-2 border border-purple-300 rounded-md focus:border-purple-500 focus:outline-none"
+            >
+              <option value="">Select a year</option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+            </select>
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="course"
+              className="block text-purple-700 font-bold mb-2"
+            >
+              Course
+            </label>
+            <select
+              id="course"
+              name="course_code"
+              value={formik.values.course_code}
+              onChange={formik.handleChange}
+              className="w-full p-2 border border-purple-300 rounded-md focus:border-purple-500 focus:outline-none"
+            >
+              <option value="">Select a course</option>
+              {formPopulation?.courses?.map((course: Course) => (
+                <option key={course.code} value={course.code}>
+                  {course.name}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="gender"
+              className="block text-purple-700 font-bold mb-2"
+            >
+              Gender
+            </label>
+            <select
+              id="gender"
+              name="gender"
+              value={formik.values.gender}
+              onChange={formik.handleChange}
+              className="w-full p-2 border border-purple-300 rounded-md focus:border-purple-500 focus:outline-none"
+            >
+              <option value="">Select a gender</option>
+              <option value="Male">Male</option>
+              <option value="Female">Female</option>
+              <option value="Other">Other</option>
+              <option value="Prefer not to say">Prefer not to say</option>
+            </select>
+          </div>
+          <button
+            type="button"
+            onClick={() => setStep(1)}
+            className="w-full bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-opacity-50"
+          >
+            Next
+          </button>
+        </>
+      )}
 
-      <div className="mb-4">
-        <label
-          htmlFor="course"
-          className="block text-purple-700 font-bold mb-2"
-        >
-          Course
-        </label>
-        <select
-          id="course"
-          name="course_code"
-          value={formik.values.course_code}
-          onChange={formik.handleChange}
-          className="w-full p-2 border border-purple-300 rounded-md focus:border-purple-500 focus:outline-none"
-        >
-          <option value="">Select a course</option>
-          {formPopulation?.courses?.map((course: Course) => (
-            <option key={course.code} value={course.code}>
-              {course.name}
-            </option>
-          ))}
-        </select>
-      </div>
+      {step === 1 && (
+        <>
+          <div className="mb-4">
+            <label className="block text-purple-700 font-bold mb-2">
+              Availability
+            </label>
+            <TimeSlotGrid
+              onChange={(newAvailability) =>
+                formik.setFieldValue("slots", newAvailability)
+              }
+            />
 
-      <div className="mb-4">
-        <label
-          htmlFor="gender"
-          className="block text-purple-700 font-bold mb-2"
-        >
-          Gender
-        </label>
-        <select
-          id="gender"
-          name="gender"
-          value={formik.values.gender}
-          onChange={formik.handleChange}
-          className="w-full p-2 border border-purple-300 rounded-md focus:border-purple-500 focus:outline-none"
-        >
-          <option value="">Select a gender</option>
-          <option value="Male">Male</option>
-          <option value="Female">Female</option>
-          <option value="Other">Other</option>
-          <option value="Prefer not to say">Prefer not to say</option>
-        </select>
-      </div>
-      <div className="mb-4">
-        <label className="block text-purple-700 font-bold mb-2">
-          Availability
-        </label>
-        <TimeSlotGrid
-          onChange={(newAvailability) =>
-            formik.setFieldValue("slots", newAvailability)
-          }
-        />
-      </div>
-      <button
-        type="submit"
-        className="w-full bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-opacity-50"
-      >
-        Submit
-      </button>
+            <div className="flex justify-between my-4">
+              <button
+                type="button"
+                onClick={() => setStep(0)}
+                className="w-1/6 bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-md focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-opacity-50"
+                style={{ marginRight: "4px" }}
+              >
+                Back
+              </button>
+              <button
+                type="submit"
+                className="w-1/2 bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-opacity-50"
+              >
+                Submit
+              </button>
+            </div>
+          </div>
+        </>
+      )}
     </form>
   );
 }
