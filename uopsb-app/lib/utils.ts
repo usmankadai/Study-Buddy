b5fec36c6de13a301ae829359063c1fbadb65ce0
usmankadai <usmankadai@gmail.com>
2024-02-29 19:13:18 +0000

    extractUpNum utility function

--- uopsb-app/lib/utils.ts
+++ uopsb-app/lib/utils.ts
@@ -4,3 +4,9 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function extractUpNum(email: string) {
+  const match = email.match(/\d+/);
+  if (!match) throw new Error("Could not extract upNum from email");
+  return match[0];
+}
