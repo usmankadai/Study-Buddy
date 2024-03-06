fce7318f387303e3c2762c80fc82b71e2a198679
usmankadai <usmankadai@gmail.com>
2024-03-06 11:15:24 +0000

    revert activeUserConfidence back to using state + add select topic dropdown #90

--- uopsb-app/app/study/page.tsx
+++ uopsb-app/app/study/page.tsx
@@ -9,12 +9,14 @@ import { TopicConfidence } from "../types";
 
 const Study = () => {
   const { user } = useAuth();
-  let activeUserConfidence: TopicConfidence[] = []; // Holds the active user's list of topics and confidence value for each 
+  const [activeUserConfidence, setActiveUserConfidence] = useState<
+    TopicConfidence[]
+  >([]); // Holds the active user's list of topics and confidence value for each
 
   useEffect(() => {
     const fetchData = async () => {
-      const userConfidence = await fetchUserConfidence(user.email);
-      activeUserConfidence = userConfidence;
+      const activeUserConfidence = await fetchUserConfidence(user.email);
+      setActiveUserConfidence(activeUserConfidence);
     };
 
     if (user) {
@@ -23,15 +25,34 @@ const Study = () => {
   }, [user]);
 
   return (
-    <div>
-      <section>
-        Your Topic Confidence:
+    <div className="container mx-auto py-8">
+      <h1 className="text-4xl font-bold mb-6">Find a Study Buddy</h1>
+      <section className="mb-8">
+        <h2 className="text-2xl font-semibold mb-4">Your Topic Confidence:</h2>
         <div className="m-4">
           <ConfidenceList confidence={activeUserConfidence} />
         </div>
       </section>
       <section>
-        <QuickFind currentUser={user} />
+        <h2 className="text-2xl font-semibold mb-4">
+          Select a Topic to Study:
+        </h2>
+        <div className="m-4">
+          <select className="border rounded p-2">
+            {activeUserConfidence.map((x) => (
+              <option key={x.topic_name} value={x.topic_name}>
+                {x.topic_name}
+              </option>
+            ))}
+            <option value="no_topic">No topic</option>
+          </select>
+        </div>
+        <h2 className="text-2xl font-semibold mb-4">
+          Match with Study Buddies:
+        </h2>
+        <div className="m-4">
+          <QuickFind currentUser={user} />
+        </div>
       </section>
     </div>
   );
