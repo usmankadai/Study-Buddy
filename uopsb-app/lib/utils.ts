282ce1c40d0ebc91bdc93838e58ceebd424270ec
usmankadai <usmankadai@gmail.com>
2024-03-07 14:59:37 +0000

    Update convertBooleanSlot to use constants #92

--- uopsb-app/lib/utils.ts
+++ uopsb-app/lib/utils.ts
@@ -1,6 +1,7 @@
 import { AvailabilitySlot } from "@/app/types";
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { days, hours } from "./constants";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -12,33 +13,34 @@ export function extractUpNum(email: string) {
   return match[0];
 }
 
-export function convertBooleanSlots(slotStates: boolean[][]): AvailabilitySlot[] {
-    try {
-      const days = ["MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"];
-      let slots: AvailabilitySlot[] = [];
-  
-      days.forEach((day, dayIndex) => {
-        let startHour: number | null = null;
-  
-        slotStates[dayIndex].forEach((hourAvailable, hourIndex) => {
-          if (hourAvailable && startHour === null) {
-            startHour = hourIndex;
-          }
-  
-          if ((!hourAvailable || hourIndex === 23) && startHour !== null) {
-            slots.push({
-              day,
-              start_hour: startHour,
-              end_hour: hourIndex,
-            });
-            startHour = null;
-          }
-        });
+export function convertBooleanSlots(
+  slotStates: boolean[][]
+): AvailabilitySlot[] {
+  try {
+    let slots: AvailabilitySlot[] = [];
+
+    days.forEach((day, dayIndex) => {
+      let startHour: number | null = null;
+
+      slotStates[dayIndex].forEach((hourAvailable, hourIndex) => {
+        if (hourAvailable && startHour === null) {
+          startHour = hours[hourIndex];
+        }
+
+        if ((!hourAvailable || hourIndex === hours.length - 1) && startHour !== null) {
+          slots.push({
+            day,
+            start_hour: startHour,
+            end_hour: hours[hourIndex],
+          });
+          startHour = null;
+        }
       });
-  
-      return slots;
-    } catch (error) {
-      console.error("Error converting Boolean slots to JSON:", error);
-      return [];
-    }
+    });
+
+    return slots;
+  } catch (error) {
+    console.error("Error converting Boolean slots to JSON:", error);
+    return [];
   }
+}
