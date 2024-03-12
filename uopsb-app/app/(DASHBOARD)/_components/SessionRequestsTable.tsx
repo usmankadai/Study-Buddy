a6a57fcf39ea2c9b8f3895cb2b9cbc667c1c1821
usmankadai <usmankadai@gmail.com>
2024-03-12 19:16:12 +0000

    add SessionRequestUser + imported SessionDate to SessionRequestsTable #95

--- uopsb-app/app/(DASHBOARD)/_components/SessionRequestsTable.tsx
+++ uopsb-app/app/(DASHBOARD)/_components/SessionRequestsTable.tsx
@@ -1,4 +1,13 @@
-const SessionRequestsTable = ({ sessionRequests }) => {
+import { SessionData } from "@/app/types";
+import SessionRequestUser from "./SessionRequestUser";
+import SessionDate from "./SessionDate";
+
+interface SessionRequestsTableProps {
+  sessionRequests: SessionData[];
+}
+const SessionRequestsTable: React.FC<SessionRequestsTableProps> = ({
+  sessionRequests,
+}) => {
   const handleAccept = (sessionID) => {
     // Handle accept action here
   };
@@ -6,7 +15,6 @@ const SessionRequestsTable = ({ sessionRequests }) => {
   const handleReject = (sessionID) => {
     // Handle reject action here
   };
-
   return (
     <section>
       <h2 className="text-xl font-semibold mb-4">Session Requests</h2>
@@ -30,12 +38,15 @@ const SessionRequestsTable = ({ sessionRequests }) => {
         <tbody className="bg-white divide-y divide-gray-200 text-xs">
           {sessionRequests.map((session, i: number) => (
             <tr key={i}>
-              <td className="px-6 py-4 whitespace-nowrap"></td>
+              <td className="px-6 py-4 whitespace-nowrap">
+                <SessionRequestUser session={session} />
+              </td>
               <td className="px-6 py-4 whitespace-nowrap">
                 <SessionDate
                   startHour={session.start_hour}
                   endHour={session.end_hour}
-                  date={session.date}></SessionDate>
+                  date={session.date}
+                ></SessionDate>
               </td>
               <td className="px-6 py-4 whitespace-nowrap">
                 <span>
@@ -65,33 +76,3 @@ const SessionRequestsTable = ({ sessionRequests }) => {
 };
 
 export default SessionRequestsTable;
-
-const SessionDate = ({ startHour, endHour, date }) => {
-  const formatDate = (dateString) => {
-    const dateObj = new Date(dateString);
-    const day = dateObj.toLocaleDateString("en-US", { weekday: "long" });
-    const month = dateObj.toLocaleDateString("en-US", { month: "long" });
-    const dayOfMonth = dateObj.getDate();
-    const ordinalSuffix =
-      dayOfMonth === 1 || dayOfMonth === 21 || dayOfMonth === 31
-        ? "st"
-        : dayOfMonth === 2 || dayOfMonth === 22
-        ? "nd"
-        : dayOfMonth === 3 || dayOfMonth === 23
-        ? "rd"
-        : "th";
-    return `${day} ${dayOfMonth}${ordinalSuffix} ${month}`;
-  };
-
-  const formatHour = (hour) => {
-    return hour >= 0 && hour <= 23
-      ? (hour % 12 || 12) + (hour < 12 ? "AM" : "PM")
-      : "";
-  };
-
-  return (
-    <div style={{ fontWeight: "bold" }}>
-      {formatDate(date)} ({formatHour(startHour)} - {formatHour(endHour)})
-    </div>
-  );
-};
