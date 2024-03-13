a6adc264f9edc67af83d8a685b1fefe1cf35b683
usmankadai <usmankadai@gmail.com>
2024-03-13 00:48:51 +0000

    fetchSessionRequests books + requests #95

--- uopsb-app/app/(DASHBOARD)/dashboard/page.tsx
+++ uopsb-app/app/(DASHBOARD)/dashboard/page.tsx
@@ -4,20 +4,29 @@ import React, { useState, useEffect } from "react";
 import StudyStatsOverview from "../_components/StudyStatsOverview";
 import { useAuth } from "@/app/AuthContext";
 import SessionRequestsTable from "../_components/SessionRequestsTable";
+import { SessionData } from "@/app/types";
 
 const Dashboard = () => {
   const { isLoggedIn, user } = useAuth();
 
-  const [sessionRequests, setSessionRequests] = useState([]);
+  const [sessionRequests, setSessionRequests] = useState<SessionData[]>([]);
+  const [sessionBookings, setSessionBookings] = useState<SessionData[]>([]);
 
   useEffect(() => {
     // Fetch the list of session requests here and set the state
     const fetchSessionRequests = async () => {
       if (!user) return;
-      const response = await fetch(`/api/session?id=${user.id}&type=requests`);
-      const data = await response.json();
-      console.log(data);
-      setSessionRequests(data);
+      const response = await fetch(`/api/session?id=${user.id}&type=all`);
+      const sessions = await response.json();
+      const requests = sessions.filter(
+        (x: SessionData) => x.status === "PENDING"
+      );
+      const bookings = sessions.filter(
+        (x: SessionData) => x.status === "ACCEPTED"
+      );
+      console.log(sessions);
+      setSessionRequests(requests);
+      setSessionBookings(bookings);
     };
     fetchSessionRequests();
   }, [user]);
@@ -28,9 +37,15 @@ const Dashboard = () => {
         <StudyStatsOverview
           studySessionsCompleted={0}
           totalStudyTime={0}
-          upcomingStudySessions={0}
+          upcomingStudySessions={sessionBookings.length}
         />
-        <SessionRequestsTable sessionRequests={sessionRequests} />
+        <SessionRequestsTable
+          sessionBookings={sessionBookings}
+          sessionRequests={sessionRequests}
+          setSessionRequests={setSessionRequests}
+          setSessionBookings={setSessionBookings}
+          currentUser={user}
+        />
       </section>
       {/* Other sections of the dashboard */}
     </section>
