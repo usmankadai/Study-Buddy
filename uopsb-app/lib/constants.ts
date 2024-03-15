73007112f726f4e2ac5e65aa68e01c2e492da4e0
usmankadai <usmankadai@gmail.com>
2024-03-15 21:07:28 +0000

    add matchTypes: MatchType to constants.ts #104

--- uopsb-app/lib/constants.ts
+++ uopsb-app/lib/constants.ts
@@ -1,3 +1,5 @@
+import { MatchType } from "@/app/types";
+
 export const initSlotStates = Array(7)
   .fill(null)
   .map(() => Array(16).fill(0));
@@ -5,3 +7,5 @@ export const initSlotStates = Array(7)
 export const days = ["MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"];
 
 export const hours = Array.from({ length: 16 }, (_, i) => i + 7);
+
+export const matchTypes: MatchType[] = ["Department", "Confidence", "Similarity"];
