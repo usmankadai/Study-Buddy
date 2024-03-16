fd2856453cc141ce5bab35ad8719feed076a537e
usmankadai <usmankadai@gmail.com>
2024-03-16 21:44:22 +0000

    Display  UserMatchCards of matched users on /study/users #104

--- uopsb-app/app/study/users/page.tsx
+++ uopsb-app/app/study/users/page.tsx
@@ -4,23 +4,56 @@ import React, { useState, useEffect } from "react";
 import { UserType } from "@/app/types";
 import { useSearchParams } from "next/navigation";
 import Link from "next/link";
+import SessionSelection from "@/app/_components/SessionSelection";
+import UserMatchCard from "@/app/_components/UserMatchCard";
+
+const defaultUser: UserType = {
+  email: "",
+  family_name: "",
+  given_name: "",
+  picture: "",
+  year: 0,
+  course_code: "",
+};
+
+type SelectedTopic = {
+  name: string;
+  id: string;
+};
+
+const defaultSelectedTopic: SelectedTopic = {
+  id: "",
+  name: "None",
+};
 
 const StudyUsers: React.FC = () => {
   const searchParams = useSearchParams();
   const [matchedUsers, setMatchedUsers] = useState<UserType[]>([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [selectedUser, setSelectedUser] = useState<UserType>(defaultUser);
+  const [selectedTopic, setSelectedTopic] =
+    useState<SelectedTopic>(defaultSelectedTopic);
+  const [showSessionSelection, setShowSessionSelection] = useState(false);
 
   useEffect(() => {
     const fetchMatchedUsers = async () => {
       const user_id = searchParams.get("id");
       const topic = searchParams.get("topic");
+      const topic_id = searchParams.get("topic_id");
       const match_type = searchParams.get("match_type");
+      if (!user_id || !topic || !topic_id || !match_type) {
+        console.log("Invalid request");
+        throw new Error(
+          "Invalid request - topic, topic_id, or match_type missing"
+        );
+      }
       const res = await fetch(
-        `/api/match?id=${user_id}&topic=${topic}&match_type=${match_type}`
+        `/api/match?id=${user_id}&topic_id=${topic_id}&match_type=${match_type}`
       );
       const data = await res.json();
       const users = data.users;
       console.log(users);
+      setSelectedTopic({ name: topic, id: topic_id });
       setMatchedUsers(users);
       setIsLoading(false);
     };
@@ -33,11 +66,51 @@ const StudyUsers: React.FC = () => {
     </div>
   ) : (
     <div>
-      <Link href="/study">Back to Search</Link>
-      <h1>Matched Users</h1>
-      {matchedUsers.map((user) => (
-        <div key={user.email}>{user.email}</div>
-      ))}
+      <h1 className="flex font-bold justify-center text-xl">Matched Users</h1>
+      <div className="flex justify-between items-center mb-4">
+        <Link
+          href="/study"
+          className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600 transition-colors"
+        >
+          Back to Search
+        </Link>
+        <div>
+          <button className="bg-green-500 text-white px-4 py-2 rounded-lg mr-2 hover:bg-green-600 transition-colors">
+            Year
+          </button>
+          <button className="bg-green-500 text-white px-4 py-2 rounded-lg hover:bg-green-600 transition-colors">
+            Course
+          </button>
+        </div>
+      </div>
+      <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-5">
+        {matchedUsers.map((user) => (
+          <div key={user.email}>
+            <UserMatchCard
+              user={user}
+              setSelectedUser={setSelectedUser}
+              setShowSessionSelection={setShowSessionSelection}
+            />
+          </div>
+        ))}
+      </section>
+      {showSessionSelection && selectedUser && (
+        <div className="fixed inset-0 z-10 bg-black bg-opacity-50 flex items-center justify-center">
+          <div className="bg-white p-8 rounded shadow-lg">
+            <button
+              onClick={() => setShowSessionSelection(false)}
+              className="absolute top-2 right-2 text-gray-500 hover:text-gray-600"
+            >
+              &times;
+            </button>
+            <SessionSelection
+              setShowSessionSelection={setShowSessionSelection}
+              selectedUser={selectedUser}
+              selectedTopic={selectedTopic}
+            />
+          </div>
+        </div>
+      )}
     </div>
   );
 };
