ddf4457ebc375a78eb0815d13ebc67b262b2a8a3
usmankadai <usmankadai@gmail.com>
2024-04-23 12:04:46 +0100

    improve and refactor email templates #134

--- uopsb-app/lib/mail/templates.ts
+++ uopsb-app/lib/mail/templates.ts
@@ -1,72 +1,51 @@
-import { SessionSlot, UserType } from "@/app/types";
+import { SessionSlot, UserSessionData, UserType } from "@/app/types";
 
 const appLink = process.env.URL;
 
-const formatSession = (session: SessionSlot) => `
-  Date: ${session.date}<br>
-  Day: ${session.day}<br>
-  Start Hour: ${session.start_hour}<br>
-  End Hour: ${session.end_hour}
+const formatSession = (session: UserSessionData) => `
+  Date: ${new Date(session.date).toLocaleDateString()}<br>
+  Time: ${session.start_hour}:00 - ${session.end_hour}:00
 `;
 
-export const requestReceivedEmail = (
+export const reqReceived = (
   user: UserType,
   topic: string,
   sessions: SessionSlot[]
 ) => `
-  <h1>New Session Request</h1>
-  <p>You have received a new session request from ${user.given_name} (${
-  user.email
+    <h1>New Session Request</h1>
+    <img src="${user.picture}" alt="${
+  user.given_name
+}'s picture" style="width: 100px; height: auto; border-radius: 50%;" />
+    <p>You have received a new session request from ${user.given_name} ${
+  user.family_name || ""
+}
 }). Here are the details:</p>
-  <p>Topic: ${topic}</p>
-  <ul>${sessions
-    .map((session) => `<li>${formatSession(session)}</li>`)
-    .join("")}</ul>
-  <p>Click <a href="${appLink}">here</a> to view the request in the app.</p>
-`;
-
-export const requestAcceptedEmail = (
-  user: UserType,
-  topic: string,
-  sessions: SessionSlot[]
-) => `
-  <h1>Session Request Accepted</h1>
-  <p>${user.given_name} (${
+    <p>Topic: ${topic}</p>
+    <ul>${sessions
+      .map((session) => `<li>${formatSession(session)}</li>`)
+      .join("")}</ul>
+    <p>Please get in touch with ${user.given_name} at ${
   user.email
-})   has accepted your session request. Here are the details:</p>
-  <p>Topic: ${topic}</p>
-  <ul>${sessions
-    .map((session) => `<li>${formatSession(session)}</li>`)
-    .join("")}</ul>
-  <p>Click <a href="${appLink}">here</a> to view the session in the app.</p>
-`;
+} to discuss the format and location of the study sessions.</p>
+    <p>Click <a href="${appLink}">here</a> to visit the app</p>
+  `;
 
-export const requestDeclinedEmail = (
+export const reqAction = (
   user: UserType,
-  topic: string,
-  sessions: SessionSlot[]
+  session: UserSessionData,
+  newStatus: string
 ) => `
-  <h1>Session Request Declined</h1>
-  <p>${user.given_name} (${
+  <h1>Booking ${newStatus}</h1>
+  <img src="${user.picture}" alt="${
+  user.given_name
+}'s picture" style="width: 100px; height: auto; border-radius: 50%;" />
+  <p>${user.given_name} ${
+  user.family_name || ""
+} has ${newStatus.toLowerCase()} the following booking:</p>
+  <p>Topic: ${session.topic_name}</p>
+  <p>${formatSession(session)}</li>
+  <p>Please contact ${user.given_name} at ${
   user.email
-}) has declined your session request. Here are the details:</p>
-  <p>Topic: ${topic}</p>
-  <ul>${sessions
-    .map((session) => `<li>${formatSession(session)}</li>`)
-    .join("")}</ul>
-  <p>Click <a href="${appLink}">here</a> to view the app.</p>
-`;
-
-export const bookingCancelledEmail = (
-  user: UserType,
-  topic: string,
-  sessions: SessionSlot[]
-) => `
-  <h1>Booking Cancelled</h1>
-  <p>${user.given_name} (${user.email}) has cancelled the following booking:</p>
-  <p>Topic: ${topic}</p>
-  <ul>${sessions
-    .map((session) => `<li>${formatSession(session)}</li>`)
-    .join("")}</ul>
-  <p>Click <a href="${appLink}">here</a> to view the app.</p>
-`;
+} to finalize the format and location of the study session.</p>
+  <p>Click <a href="${appLink}">here</a> to visit the app</p>
+  `;
